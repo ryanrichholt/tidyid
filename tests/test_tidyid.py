43173f406a182fbb1ddb65146bfdc159f3e2cf00
Ryan Richholt <ryanrichholt@users.noreply.github.com>
2020-02-13 22:36:06 -0700

    Update test_tidyid.py

--- tests/test_tidyid.py
+++ tests/test_tidyid.py
@@ -2,7 +2,12 @@ import pytest
 import tidyid
 
 
-@pytest.mark.parametrize('x', range(1000))
+def test_int_to_tid_0():
+    with pytest.raises(TypeError):
+        tidyid.int_to_tid(0)
+
+
+@pytest.mark.parametrize('x', range(1, 1000))
 def test_int_to_tid(x):
     tidyid.int_to_tid(x)
     
